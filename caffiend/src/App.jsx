82da3f7e82eb22e05fd4fb02ed986b68596b681f
aridepai17 <advaithdepai26@gmail.com>
2025-05-16 19:19:21 +0530

    Hero.jsx file completion

--- caffiend/src/App.jsx
+++ caffiend/src/App.jsx
@@ -1,12 +1,27 @@
-
+import CoffeeForm from './components/CoffeeForm.jsx'
+import Layout from './components/Layout.jsx'
+import Hero from './components/Hero.jsx'
+import Stats from './components/Stats.jsx'
+import History from './components/History.jsx'
 
 
 function App() {
 
-  return (
+  const isAuthenticated = false
+
+  const authenticatedContent = (
     <>
-      
+      <Stats />
+      <History />
     </>
+  )
+
+  return (
+    <Layout>
+      <Hero />
+      <CoffeeForm />
+      { isAuthenticated && (authenticatedContent) }
+    </Layout>
   )
 }
 
